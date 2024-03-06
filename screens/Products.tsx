c18b88690c370e15561ce2444ca4be6950c2e867
Mohd Ziyad <mziyadc@gmail.com>
2024-03-06 20:57:48 +0530

    Initial commit

---
+++ screens/Products.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Products = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Product Screen!</Text>
+    </View>
+  );
+};
+
+export default Products;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+});
