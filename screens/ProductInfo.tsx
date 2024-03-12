82d7d01dea9c8989fb9d4f67d41229f33b2b7343
Mohd Ziyad <mziyadc@gmail.com>
2024-03-12 10:15:13 +0530

    Icon to nav back added in product info

--- screens/ProductInfo.tsx
+++ screens/ProductInfo.tsx
@@ -1,22 +1,58 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
-import { RouteProp } from "@react-navigation/native";
+import { RouteProp, useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../App";
+import { useProduct } from "medusa-react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Feather } from "@expo/vector-icons";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 type ProductInfoRouteProp = RouteProp<RootStackParamList, "ProductInfo">;
 
 type ProductInfoProps = {
   route: ProductInfoRouteProp;
 };
+type ParamList = NativeStackNavigationProp<RootStackParamList, "ProductInfo">;
 const ProductInfo = ({ route }: ProductInfoProps) => {
   const { id } = route.params;
+
+  const { product, isLoading } = useProduct(id);
+  const { goBack } = useNavigation<ParamList>();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>ProductInfo with id: {id}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size={"large"} color={"violet"} />
+        </View>
+      ) : (
+        <View>
+          <TouchableOpacity
+            onPress={() => {
+              goBack();
+            }}
+          >
+            <Feather name="chevron-left" size={40} color="black" />
+          </TouchableOpacity>
+        </View>
+      )}
+    </SafeAreaView>
   );
 };
 
 export default ProductInfo;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+});
